a22a7db20eac02f82d7c2a118799d3f4712e75bc
Dariusz Okonski <darek200180@gmail.com>
2022-09-07 21:42:20 +0100

    add useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -9,7 +9,28 @@ let initialState = {
 };
 
 const firestoreReducer = (state, action) => {
-    switch (action.type) {        
+    switch (action.type) {
+        case 'IS_PENDING':
+            return { 
+                inPending: true ,
+                document: null,
+                success: false,
+                error: null,
+            }        
+        case 'ADDED_DOCUMENT':
+            return { 
+                isPending: false, 
+                document: action.payload, 
+                success: true, 
+                error: null 
+            }
+        case 'ERROR':
+            return { 
+                isPending: false, 
+                document: null, 
+                error: action.payload, 
+                success: false
+            }
         default:
             return state;
     }
@@ -17,16 +38,30 @@ const firestoreReducer = (state, action) => {
 
 export const useFirestore = (collection) => {
     const [response, dispatch] = useReducer(firestoreReducer, initialState)   
-    const [isCanceled, setIsCanceled] = useState(false);
+    const [, setIsCanceled] = useState(false);
 
     // collection ref
     const ref = projectFirestore.collection(collection);
 
-    const addDocument = (doc) => {
-
+    // only dispatch if not canceled
+    const dispatchIfNotCancel = (action) => {
+        if (!isCanceled) {
+            dispatch(action)
+        }
     }
 
-    const deleteDocument = (id) => {
+    const addDocument = async (doc) => {
+        dispatch({ type: 'IS_PENDING'})
+
+        try {
+            const addedDocument = await ref.add(doc);            
+            dispatchIfNotCancel({ type: 'ADDED_DOCUMENT', payload: addDocument })
+        } catch (err) {
+            dispatchIfNotCancel({ type: 'ERROR', payload: err.message})
+        }
+    }
+
+    const deleteDocument = async (id) => {
 
     }
 
