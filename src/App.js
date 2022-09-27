0eaef852a54171c032c9d287f2ffb2d22127cd05
Dariusz Okonski <darek200180@gmail.com>
2022-09-27 23:28:29 +0200

    start from scratch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import { useAuthContext } from './hooks/useAuthContext';
-
-
 function App() {
-  const { authIsReady, user } = useAuthContext();
-  
   return (
     <div className="App">
-        {authIsReady && 
-        (<BrowserRouter>
-            <Navbar />          
-
-            <Routes>
-              <Route path='/' element={user ? <Home /> : <Navigate to="/login" />} />
-              <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
-              <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/" />} />
-              <Route path='*' element={<Navigate to="/" />} />
-            </Routes>
-            
-        </BrowserRouter>)}
-      </div>
+      <h1>Hello world</h1>
+    </div>
   );
 }
 
