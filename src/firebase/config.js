19ced7bac07c532eec1310a1997db617029ec2fd
Dariusz Okonski <darek200180@gmail.com>
2022-09-28 22:03:18 +0200

    add config file

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Ts26AcP3V-i0OEcJIt4N0rQZ7wSEoaY",
+  authDomain: "mymoney-v1-542f4.firebaseapp.com",
+  projectId: "mymoney-v1-542f4",
+  storageBucket: "mymoney-v1-542f4.appspot.com",
+  messagingSenderId: "514015679640",
+  appId: "1:514015679640:web:ab47718d713482f0f0eced",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
