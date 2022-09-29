6f1fc93690f5ef05702137bf8a650b7a245bdae0
Dariusz Okonski <darek200180@gmail.com>
2022-09-29 09:19:24 +0200

    signup user

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,15 +1,17 @@
 import styles from "./Signup.module.css";
 import { useState } from "react";
+import { useSignup } from "./../../hooks/useSignup";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
+  const { signup, isPending, error } = useSignup();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(email, password, displayName);
+    signup(email, password, displayName);
   };
 
   return (
@@ -41,8 +43,13 @@ export default function Signup() {
           value={displayName}
         />
       </label>
-
-      <button className="btn">Signup</button>
+      {!isPending && <button className="btn">Signup</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 }
